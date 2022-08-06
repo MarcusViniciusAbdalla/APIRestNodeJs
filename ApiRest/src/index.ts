a47fff4eae0843c2502c2d7bfabd94988a410054
Marcus Vinicius Abdalla <93200321+MarcusViniciusAbdalla@users.noreply.github.com>
2022-08-05 22:06:25 -0300

    Update index.ts

--- ApiRest/src/index.ts
+++ ApiRest/src/index.ts
@@ -3,12 +3,17 @@ import usersRoute from "./routes/users.route";
 
 const app = express();
 
+// Configuração da aplicação
+app.use(express.json());
+
+// Configuração de rotas
 app.use(usersRoute);
 
 app.get('/status', (req: Request, res: Response, next: NextFunction) => {
     res.status(200).send({ foo: 'Sucesso!' });
 });
 
+// Inicialização do servidor
 app.listen(3000, () => {
    console.log('Aplicação executando na porta 3000');
 });
