339823586b5d9bf821fc8a2fccb1624f5c359894
Marcus Vinicius Abdalla <93200321+MarcusViniciusAbdalla@users.noreply.github.com>
2022-08-05 22:51:45 -0300

    Update index.ts

--- ApiRest/src/index.ts
+++ ApiRest/src/index.ts
@@ -1,5 +1,6 @@
 import express, {Request, Response, NextFunction} from "express";
 import usersRoute from "./routes/users.route";
+import statusRoute from "./routes/status.route";
 
 const app = express();
 
@@ -10,9 +11,7 @@ app.use(express.urlencoded({ extended: true }));
 // Configuração de rotas
 app.use(usersRoute);
 
-app.get('/status', (req: Request, res: Response, next: NextFunction) => {
-    res.status(200).send({ foo: 'Sucesso!' });
-});
+app.use(statusRoute);
 
 // Inicialização do servidor
 app.listen(3000, () => {
